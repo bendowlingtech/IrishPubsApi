63de62d423e260ccd0048d4998aae885f2afef0e
Ben.Dowling <bendowlingtech@gmail.com>
2022-02-04 18:33:42 +0000

    Inital project with some endpoints added

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/irishpubs/api/all")
+async def get_pubs():
+    return {"message": "Hello"}
+
+
+@app.get("/irishpubs/api/{name}")
+async def get_pub(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/irishpubs/api/{county}")
+async def get_pub(name: str):
+    return {"message": f"Hello {name}"}
